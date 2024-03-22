a9d14a7ab50395d84d996c20e6e5091b321b9b39
manjimaprasad22 <manjimaprasad22@gmail.com>
2024-03-22 16:38:09 +0530

    youtube

--- src/components.jsx/cards/Cards.jsx
+++ src/components.jsx/cards/Cards.jsx
@@ -3,10 +3,12 @@ import data from './assets/Data.json'
 import thaj from './assets/images/thaj.jpg'
 import { useSelector } from 'react-redux';
 import './assets/css/style.css'
+import { useNavigate } from 'react-router-dom';
 
 const Cards = () => {
 console.log(data,"aaaaa");
 const { carbon } = useSelector((state) => state.carbon);
+const navigate = useNavigate()
   return (
     <div>
         <div class="card" style={{width: "18rem"}} >
@@ -19,7 +21,7 @@ const { carbon } = useSelector((state) => state.carbon);
   <div class="card-body">
     <h5 class="card-title">{data.data.name}</h5>
     <p class="card-text">{data.data.description}</p>
-    <button class="btn btn-primary">Go somewhere</button>
+    <button class="btn btn-primary" onClick={()=>navigate('/detail')}>Detail</button>
   </div>
 </div>
     </div>
--- src/views/Detailpage.jsx
+++ src/views/Detailpage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const Detailpage = () => {
   return (
-    <div>
-        
+      <div>
+        <Container>
+        {/* <div className="ratio ratio-16x9">
+  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowFullScreen></iframe>
+</div> */}
+<iframe width="560" height="315" src="https://www.youtube.com/embed/8XbTb9yt0Ls?si=AR7iJOAO3pYu-tuZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>    </Container>
     </div>
   )
 }
